bd0dd0d0ac04fa81a0c775a9664e6314d6b3d243
Nazar Chopko <nazarchopko@MacBook-Pro-Nazar.local>
2024-05-01 17:49:54 +0300

    homework_4

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,42 @@
 const { bgRed, bgYellow, bgGreen } = require("colors");
 const config = require("config");
+const fs = require("fs");
+const path = require("path");
 
+const date = new Date();
+const rootDir = path.join(__dirname, "../");
 const logLevel = config.logLevel;
 
+fs.mkdirSync(path.join(rootDir, "logs"), { recursive: true });
+
+const writeInfoLogStream = fs.createWriteStream(
+  path.join(rootDir, "/logs/info.txt")
+);
+const writeErrorLogStream = fs.createWriteStream(
+  path.join(rootDir, "/logs/error.txt")
+);
+
 const info = (fileName, message) => {
+  writeInfoLogStream.write(`${date.toISOString()}: ${message}\n`);
   if (logLevel !== "info") return;
   console.log(`${fileName} ${message}`);
 };
 
 const warn = (fileName, message) => {
+  writeErrorLogStream.write(`${date.toISOString()}: ${message}\n`);
   if (logLevel === "error") return;
   console.error(`${fileName} ${message}`);
 };
 
 const error = (fileName, message) => {
+  writeErrorLogStream.write(`${date.toISOString()}: ${message}\n`);
   console.error(`${fileName} ${message}`);
 };
+
+process.on("beforeExit", () => {
+  writeInfoLogStream.end();
+  writeErrorLogStream.end();
+});
 
 const logger = (executedFileName) => {
   return {
